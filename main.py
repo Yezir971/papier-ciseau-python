f3aeb5b4e8ba55e89ad2d610c2f0cb7172799c79
Yezir971 <james_ahmedaly@yahoo.com>
2025-11-07 16:44:29 +0100

    feat : add front menu game

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
+from app.utils import *
 def main():
-    print('')
+ 
+    is_true = True
+    while is_true == True:
+        try: 
+            interface_menu_game()
+            choix = choice_player_dificulty()
+            if(choix == "quitter"):
+                print('quitter')
+                return
+            if(int(choix) == 1):
+                print(f'difficulté -> {choix}')
+                
+            elif (int(choix) == 2):
+                print(f'cdifficulté -> {choix}')
+        except:
+            print('Vous avez entrer un mauvais choix')
+            exit()
+        # else:
+        #     is_true = False
+        #     print('mauvais choix')
+    # print(choix)
+    
     
     
 if __name__ == "__main__":
